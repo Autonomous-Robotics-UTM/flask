a42bc81fe0d22d1120dc651082c151c2e709f04a
josh <joshua.mazariegosbobadilla@mail.utoronto.ca>
2020-02-04 16:16:44 -0500

    clear button now

--- server.py
+++ server.py
@@ -16,7 +16,8 @@ dash_app.layout = html.Div([
             id='interval-component',
             interval=1000, # in milliseconds
             n_intervals=0
-        )
+        ),
+    html.Button('Clear', id='button')
 ])
 
 csv_name = str(int(time.time())) + '.csv'
@@ -25,19 +26,33 @@ with open(csv_name, 'w') as file:
     writer.writerow(["Time", "Steer Command"])
 max_t = 5
 
+def get_figure(data):
+    fig = {
+        'data': [{'type': 'line',
+                  'x': data['Time'],
+                  'y': data['Steer Command']}],
+        'layout': {'title': {'text': 'Steer Commands over Time'},
+                   'yaxis': [-1, 1]}
+    }
+    return fig
+
+#dash callbacks
 @dash_app.callback(
     Output(component_id='graph', component_property='figure'),
     [Input(component_id='interval-component', component_property='n_intervals')]
 )
 def update_output_div(n):
-    data = pd.read_csv(csv_name)
-    fig = {
-        "data": [{"type": "line",
-                  "x": data['Time'],
-                  "y": data['Steer Command']}],
-        "layout": {"title": {"text": "Steer Commands over Time"}}
-    }
-    return fig
+    return get_figure(pd.read_csv(csv_name))
+
+@dash_app.callback(
+    Output(component_id='interval-component', component_property='n_intervals'),
+    [Input(component_id='button', component_property='n_clicks')]
+)
+def update_output_div(n):
+    with open(csv_name, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Time", "Steer Command"])
+    return 0
 
 #routes
 @app.route('/')
@@ -54,4 +69,4 @@ def graph_post():
     return Response('Data Read!', headers={'status': 200})
 
 if __name__ == "__main__":
-    dash_app.run_server(port=8080)
+    dash_app.run_server(host='0.0.0.0', port=8080)
